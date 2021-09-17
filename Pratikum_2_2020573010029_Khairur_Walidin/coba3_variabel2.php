6765884028b56446755420731cbd0bfff8d377cc
Khairur <walidinkhairul29@gmail.com>
2021-09-17 17:17:43 +0700

    pemakaain variabel contoh 2

---
+++ Pratikum_2_2020573010029_Khairur_Walidin/coba3_variabel2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $jalan ="medan - B.Aceh";
+        $noRumah ="29";
+        $blok ="4C";
+        $jumlahpenghuni = 3;
+        echo "Hasil Variabel Variabel jalan + Variabel noRumah adalah";
+        echo $alamat = $jalan . $noRumah;
+        echo "<br>";
+        echo "Hasil Variabel Variabel noRumah + Variabel jumlahpenghuni adalah";
+        echo $hasil = $noRumah + $jumlahpenghuni;
+        echo "<br>";
+        echo "Hasil Variabel Variabel blok + Variabel noRumah adalah";
+        echo $hasil = $hasil + $noRumah;
+        echo "<br>";
+    ?>
+    
+</body>
+</html>
